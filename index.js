c169cb3cacf4737cb36c544f623179f2e4322516
Cedricsakah5 <125036359+Cedricsakah5@users.noreply.github.com>
2023-09-05 15:06:38 +0100

    IPRO-28: creating a user (#1)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res)=>{
     res.send('Hello world')
+    
 })
 app.listen(5500, () => console.log('listening on port 5500...'))
