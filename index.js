6b0e4b8915019be32bcca1dfc5153a29a2c90053
sakah Cedric <cedricsakah5@gmail.com>
2023-11-16 21:52:43 +0100

    authentication endpoint.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const user = require('./routes/Users');
 const property = require('./routes/Properties')
 const Apartment = require('./routes/Apartments');
 const Rental = require('./routes/Rental')
+const auth = require('./routes/auth')
 const express = require('express');
 
 const app = express();
@@ -19,6 +20,7 @@ app.use('/api/users', user);
 app.use('/api/properties', property);
 app.use('/api/apartments', Apartment);
 app.use('/api/rentals', Rental);
+app.use('/api/auth',auth)
 
 app.listen(3000, () => console.log('listening on port 3000...'));
 
--- models/property.js
+++ models/property.js
@@ -28,6 +28,10 @@ const Property =  mongoose.model('Property', new mongoose.Schema({
     image:{
         data:Buffer,
         contentType:String
+    },
+    date:{
+        type:Date,
+        default:Date.now
     }
 }));
 
@@ -37,7 +41,8 @@ function validateProperty(property) {
       name: Joi.string().min(3).max(250).required(),
       description: Joi.string().min(5).max(500).required(),
       ownerId: Joi.string().required(),
-      location: Joi.string().required()
+      location: Joi.string().required(),
+      date: Joi.date().required()
 
     });
     return schema.validate(property);
